89fc4df81ae371deddbab5c5f51fc65a89778866
guimaraesalves <guimaraesalves45@gmail.com>
2021-10-26 20:44:37 -0300

    commit entrega preliminar

--- atvidade-lista-de-frutas/src/App.js
+++ atvidade-lista-de-frutas/src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [fruits, setFruits] = useState([
+    { name: "banana", color: "yellow", price: 2 },
+    { name: "cherry", color: "red", price: 3 },
+    { name: "strawberry", color: "red", price: 4 },
+  ]);
+
+  const fruitNames = fruits.map((item) => {
+    return <li>{item.name}</li>;
+  });
+
+  const filterRedFruits = fruits.filter((item) => {
+    return item.color === "red";
+  });
+
+  const totalPrice = fruits.reduce((vlrAnterior, vlrAtual) => {
+    return vlrAtual.price + vlrAnterior;
+  }, 0);
+
+  function addFruit() {
+    setFruits(filterRedFruits);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fruits</h1>
+        <p>Preço Total = R$ {totalPrice}</p>
+        <div>
+          <ul>
+            <li>{fruitNames}</li>
+          </ul>
+        </div>
+        <button className="btn" onClick={addFruit}>
+          show red fruits
+        </button>
       </header>
     </div>
   );
---
+++ atvidade-lista-de-frutas/src/components/FruitList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FruitList() {
+  return <div></div>;
+}
+
+export default FruitList;
