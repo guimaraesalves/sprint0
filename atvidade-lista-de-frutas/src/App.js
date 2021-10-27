4745e301e58be8d405b91c0f8cb4204eb5fe5a28
guimaraesalves <guimaraesalves45@gmail.com>
2021-10-27 11:28:57 -0300

    commit first  delivery atividade fruit

--- atvidade-lista-de-frutas/src/App.js
+++ atvidade-lista-de-frutas/src/App.js
@@ -1,46 +1,18 @@
 import "./App.css";
 import { useState } from "react";
+import FruitList from "./components/FruitList";
 
 function App() {
   const [fruits, setFruits] = useState([
-    { name: "Banana", color: "yellow", price: 2 },
-    { name: "Cherry", color: "red", price: 3 },
-    { name: "Strawberry", color: "red", price: 4 },
+    { name: "Banana 🍌 ", color: "yellow", price: 2 },
+    { name: "Cherry 🍒 ", color: "red", price: 3 },
+    { name: "Strawberry 🍓 ", color: "red", price: 4 },
   ]);
-
-  const fruitNames = fruits.map((item) => {
-    return (
-      <li>
-        {item.name} - R$ {item.price}
-      </li>
-    );
-  });
-
-  const filterRedFruits = fruits.filter((item) => {
-    return item.color === "red";
-  });
-
-  const totalPrice = fruits.reduce((vlrAnterior, vlrAtual) => {
-    return vlrAtual.price + vlrAnterior;
-  }, 0);
-
-  function addFruit() {
-    setFruits(filterRedFruits);
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Fruits</h1>
-        <p>Preço Total = R$ {totalPrice}</p>
-        <div>
-          <ul>
-            <li>{fruitNames}</li>
-          </ul>
-        </div>
-        <button className="btn" onClick={addFruit}>
-          show red fruits
-        </button>
+        <FruitList fruits={fruits} setFruits={setFruits} />
       </header>
     </div>
   );
--- atvidade-lista-de-frutas/src/components/FruitList.jsx
+++ atvidade-lista-de-frutas/src/components/FruitList.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-function FruitList() {
-  return <div></div>;
+function FruitList(props) {
+  const fruitNames = props.fruits.map((item) => {
+    return (
+      <li>
+        {item.name} - R$ {item.price}
+      </li>
+    );
+  });
+
+  const filterRedFruits = props.fruits.filter((item) => {
+    return item.color === "red";
+  });
+
+  const totalPrice = props.fruits.reduce((vlrAnterior, vlrAtual) => {
+    return vlrAtual.price + vlrAnterior;
+  }, 0);
+
+  function addFruit() {
+    props.setFruits(filterRedFruits);
+  }
+  return (
+    <div>
+      <h1>Fruits</h1>
+      <p>Preço Total = R$ {totalPrice}</p>
+      <div>
+        <ul>
+          <li>{fruitNames}</li>
+        </ul>
+      </div>
+      <button className="btn" onClick={addFruit}>
+        show red fruits
+      </button>
+    </div>
+  );
 }
 
 export default FruitList;
