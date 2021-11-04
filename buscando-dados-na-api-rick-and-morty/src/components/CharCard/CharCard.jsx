5a6b1401f6a0ab4c5bac017d0424382d170abd36
guimaraesalves <guimaraesalves45@gmail.com>
2021-11-03 22:46:30 -0300

    commit include status

--- buscando-dados-na-api-rick-and-morty/src/components/CharCard/CharCard.jsx
+++ buscando-dados-na-api-rick-and-morty/src/components/CharCard/CharCard.jsx
@@ -5,6 +5,7 @@ function CharCard({ char }) {
     <div key={char.id} className={`card ${char.status}`}>
       <label>{char.name}</label>
       <img className="picture" src={char.image} alt={char.name}></img>
+      <label className="status">{char.status}</label>
     </div>
   );
 }
