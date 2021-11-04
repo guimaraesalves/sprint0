55e4f9ccbd53d6640166bdc98bd55f9f5abd590c
guimaraesalves <guimaraesalves45@gmail.com>
2021-11-04 15:10:24 -0300

    commit first correction

--- buscando-dados-na-api-rick-and-morty/src/components/CharCard/CharCard.jsx
+++ buscando-dados-na-api-rick-and-morty/src/components/CharCard/CharCard.jsx
@@ -2,11 +2,14 @@ import "./CharCard.css";
 
 function CharCard({ char }) {
   return (
+    
+      
     <div key={char.id} className={`card ${char.status}`}>
-      <label>{char.name}</label>
-      <img className="picture" src={char.image} alt={char.name}></img>
+      <div className="card-title">{char.name}</div>
+      <img className="card-body" src={char.image} alt={char.name}></img>
       <label className="status">{char.status}</label>
     </div>
+    
   );
 }
 
